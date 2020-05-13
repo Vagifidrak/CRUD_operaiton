1b80912fa3a509084484a5eecd9fb0b10a719d99
Vagif Ibrahimov <vaqa.ibrahim@gmail.com>
2020-05-14 00:25:48 +0400

    CRUD Operation2

---
+++ StoreASP/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using Store.Services;
+using StoreEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StoreASP.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductServices productServices = new ProductServices();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ProductTable(string search)
+        {
+            var products = productServices.GetProducts();
+            if (!string.IsNullOrEmpty(search))              //Nida işarəsi əvvəldə olduqda deyilsə əvəz edir
+            {
+                products = products.Where(pr => pr.Name.ToLower().Contains(search)).ToList();  
+            }
+            return PartialView(products);
+        }
+        public ActionResult Create()
+        {
+            return PartialView();
+        }
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            productServices.SaveProduct(product);
+            return RedirectToAction("ProductTable");
+        }
+    }
+}
